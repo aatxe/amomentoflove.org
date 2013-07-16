336080e28eeb33d38ead4ac8762e0f23b5298558
Aaron Weiss <aaronweiss74@gmail.com>
2013-07-16 17:13:52 -0400

    Starting with a really basic transitioning engine.

---
+++ js/mol.js
@@ -0,0 +1,24 @@
+$(function() {
+	var $window = $(window),
+		$doc = $(document),
+		$html = $('html'),
+		$body = $('body'),	
+		$container = $('#container');
+		
+	$doc.on('keydown', function(event) {
+		if (event.which == 32) {
+			$window.trigger('advance');
+		};
+	});
+	
+	$window.on('advance', function() {
+		var $frame = $container.find('.frame'),
+			$current = $container.find('.current'),
+			$next = $frame.eq($current.index() + 1);
+			
+		if (Modernizr.csstransitions) {
+			$current.removeClass('current').addClass('spent');
+			$next.addClass('current');
+		}
+	});
+});
